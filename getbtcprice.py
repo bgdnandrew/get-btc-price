c389fa7037d6042400441b5f42b1624da68d44d4
Bogdan Andrei <bogdanandrei.online@gmail.com>
2023-08-13 15:28:55 +0300

    implemented the new auth and create_tweet methods suggested in tweepy 4.X; better print statements for better logs. requirements.txt now encoded UTF-8.

--- getbtcprice.py
+++ getbtcprice.py
@@ -1,33 +1,49 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 import tweepy
 import time
 import requests
 
+load_dotenv()
 
 API_KEY             = os.getenv('API_KEY')
-API_SECRET_KEY      = os.getenv('API_SECRET_KEY')
+API_KEY_SECRET      = os.getenv('API_KEY_SECRET')
 BEARER_TOKEN        = os.getenv('BEARER_TOKEN')
 ACCESS_TOKEN        = os.getenv('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
 
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+# deprecated
+# -----------------------------
+# auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+# auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+# api = tweepy.API(auth)
+# -----------------------------
 
+# new method according to tweepy 4.X
+client = tweepy.Client(bearer_token = BEARER_TOKEN , 
+                       consumer_key = API_KEY, 
+                       consumer_secret = API_KEY_SECRET, 
+                       access_token = ACCESS_TOKEN, 
+                       access_token_secret = ACCESS_TOKEN_SECRET)
 
-def get_btc():
+def get_btc_value():
     response = requests.get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
+    print("Response: " + str(response.status_code) + " " + response.reason)
     data = response.json()
-    currency = data["data"]["base"]
+    cryptocurrency = data["data"]["base"]
     price = data["data"]["amount"]
-    print ("Currency: " +  currency + " Price: " + price)
-    api.update_status('#BTC is now at $' + price)
+    print ("Cryptocurrency: " +  cryptocurrency + " Price: " + price)
+
+    # deprecated
+    # api.update_status('#BTC is now at $' + price)
+
+    # new method according to tweepy 4.X
+    tweet_string = "#BTC is now at $" + price
+    client.create_tweet(text = tweet_string)
+    print("Tweeted: ", tweet_string)
 
 while 1==1:
     time.sleep(10)
-    get_btc()
-    time.sleep(1200)
+    get_btc_value()
+    time.sleep(1500)
